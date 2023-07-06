c28a9f7cf95ffad00e934fb918052d30a0040c9a
Ulf Tigerstedt ulf.tigerstedt@csc.fi <ulf.tigerstedt@csc.fi>
2023-07-06 12:05:38 +0300

    More linting, added pip upgrade

--- one_pass/convert_time.py
+++ one_pass/convert_time.py
@@ -3,16 +3,16 @@ import numpy as np
 
 """Functions to convert time for the one pass algorithms."""
 """A mapping between time words (and time stamps) and number of minutes,
-# to avoid having multiple if statements. Converts to minutes expect for monthly, 
+# to avoid having multiple if statements. Converts to minutes expect for monthly,
 where you need to check the number of days in a month. This is done below.
 
-Inputs: 
-time_word - a word specifiying a frequency  
-time_stamp_input - a pandas timestamp of the current data 
+Inputs:
+time_word - a word specifiying a frequency
+time_stamp_input - a pandas timestamp of the current data
 
-Outputs: 
+Outputs:
 stat_freq_min - the total number of minutes corresponding to the given 'time_word' (for montly, this will depend on the incoming timestamp)
-time_stamp_input_tot - the total number of minutes of the incoming timestamp since the start of the given time word frequency 
+time_stamp_input_tot - the total number of minutes of the incoming timestamp since the start of the given time word frequency
 time_stamp_tot_append - the number of units (given by time word) of the current time stamp - used for appending data in time_append
 """
 
@@ -34,14 +34,14 @@ def convert_time(time_word = "daily", time_stamp_input = None):
 
     Args:
         time_word: the input saving frequency.
-        time_stamp_input: input timestamp of the incoming data 
+        time_stamp_input: input timestamp of the incoming data
     """
 
-    #### first looking at requested stat freq #### 
+    #### first looking at requested stat freq ####
     if time_word not in times:
         valid_values = ", ".join(times.keys())
         raise ValueError(f"The input saving frequency '{time_word}' is not supported, valid values are: {valid_values}")
-    
+
     if time_word == "continuous":
         raise ValueError(f"Can not put continuous as the output frequency, must specifcy frequency (e.g. monthly) for on-going netCDF files")
 
@@ -54,52 +54,52 @@ def convert_time(time_word = "daily", time_stamp_input = None):
     if time_word == "monthly":
         stat_freq_min = time_stamp_input.days_in_month * stat_freq_min
     elif time_word == "3monthly":
-        stat_freq_min = stat_freq_min * (time_stamp_input.days_in_month + 
-                               pd.Timestamp(year = time_stamp_input.year, month = time_stamp_input.month + 1, day = 1).days_in_month + 
+        stat_freq_min = stat_freq_min * (time_stamp_input.days_in_month +
+                               pd.Timestamp(year = time_stamp_input.year, month = time_stamp_input.month + 1, day = 1).days_in_month +
                                pd.Timestamp(year = time_stamp_input.year, month = time_stamp_input.month + 2, day = 1).days_in_month)
 
 
-    #### now looking at given time stamp #### 
-    # converting everything into minutes 
+    #### now looking at given time stamp ####
+    # converting everything into minutes
     time_stamp_input_min = time_stamp_input.minute
-    time_stamp_input_hour = time_stamp_input.hour*60 
+    time_stamp_input_hour = time_stamp_input.hour*60
     time_stamp_input_day = (time_stamp_input.day-1)*24*60
     time_stamp_input_day_of_week = (time_stamp_input.day_of_week)*24*60
     time_stamp_input_month = (time_stamp_input.month- 1)* time_stamp_input.days_in_month*24*60
     time_stamp_input_year = time_stamp_input.year*365*24*60
 
     if(time_word == "hourly"):
-        
+
         time_stamp_input_tot = time_stamp_input_min
         time_stamp_tot_append = time_stamp_input_hour
 
     elif(time_word == "3hourly"):
-        
+
         if(np.mod(time_stamp_input.hour, 3) != 0):
             time_stamp_input_tot = time_stamp_input_min + time_stamp_input_hour
         else:
             time_stamp_input_tot = time_stamp_input_min
-        
+
         time_stamp_tot_append = time_stamp_input_hour
 
     elif(time_word == "6hourly"):
-        
+
         if(np.mod(time_stamp_input.hour, 6) != 0):
             time_stamp_input_tot = time_stamp_input_min + time_stamp_input_hour
         else:
             time_stamp_input_tot = time_stamp_input_min
-        
+
         time_stamp_tot_append = time_stamp_input_hour
 
     elif(time_word == "12hourly"):
-        
+
         if(np.mod(time_stamp_input.hour, 12) != 0):
             time_stamp_input_tot = time_stamp_input_min + time_stamp_input_hour
         else:
             time_stamp_input_tot = time_stamp_input_min
-        
+
         time_stamp_tot_append = time_stamp_input_hour
-        
+
 
     elif(time_word == "daily"):
 
@@ -109,11 +109,11 @@ def convert_time(time_word = "daily", time_stamp_input = None):
     elif(time_word == "weekly"):
 
         time_stamp_input_tot = time_stamp_input_min + time_stamp_input_hour + time_stamp_input_day_of_week
-        time_stamp_tot_append = time_stamp_input.week*24*60 # this gives you the week of the year? 
+        time_stamp_tot_append = time_stamp_input.week*24*60 # this gives you the week of the year?
 
     elif(time_word == "monthly"):
 
-        time_stamp_input_tot = time_stamp_input_min + time_stamp_input_hour + time_stamp_input_day 
+        time_stamp_input_tot = time_stamp_input_min + time_stamp_input_hour + time_stamp_input_day
         time_stamp_tot_append = time_stamp_input_month
 
     if(time_word == "3monthly"):
@@ -129,6 +129,6 @@ def convert_time(time_word = "daily", time_stamp_input = None):
 
         time_stamp_input_tot = time_stamp_input_min + time_stamp_input_hour + time_stamp_input_day + time_stamp_input_month
         time_stamp_tot_append = time_stamp_input_year
-        #TODO: finish time_stamp_tot_append 
+        #TODO: finish time_stamp_tot_append
 
     return stat_freq_min, time_stamp_input_tot, time_stamp_tot_append
