bab93f99f65026cd4814ade4e5c8b1db0c837180
kgrayson <katherine.grayson@bsc.es>
2023-08-30 17:55:09 +0200

    updating to version 4.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ tests = [
 
 setup(
     name="one_pass",
-    version="0.4.0",
+    version="0.4.1",
     description="One Pass Algorithms",
     author="Katherine Grayson",
     author_email="katherine.grayson@bsc.es",
