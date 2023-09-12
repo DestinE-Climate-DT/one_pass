f41dd3aa3f78c05dcbc2705fd7ffd0f81327a6ca
kgrayson <katherine.grayson@bsc.es>
2023-09-12 16:38:11 +0200

    adding correct path for data retrival

--- tests/get_data.sh
+++ tests/get_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -nc -O pr_12_months.nc "https://zenodo.org/record/8337510/files/pr_12_months.nc?download=1"
+wget -nc -O tests/pr_12_months.nc "https://zenodo.org/record/8337510/files/pr_12_months.nc?download=1"
 
