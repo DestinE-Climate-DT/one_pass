e10676b53bab87d190156ed3beee4f851dc37181
kgrayson <katherine.grayson@bsc.es>
2023-10-06 12:39:02 +0200

    trying to fix setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "dask",
         "zarr",
         "cython",
-        "git+https://github.com/dask/crick.git@main",
+        "crick",
         "pytest",
         "netcdf4",
         "cytoolz",
